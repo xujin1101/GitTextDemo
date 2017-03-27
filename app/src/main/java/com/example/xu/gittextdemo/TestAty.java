9b0f5f024dafc90daa67658356fcfad42376fffc
Administrator <xujin6011@163.com>
2017-03-27 22:29:13 +0800

    第一次

---
+++ app/src/main/java/com/example/xu/gittextdemo/TestAty.java
@@ -0,0 +1,18 @@
+package com.example.xu.gittextdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+
+public class TestAty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
